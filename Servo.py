5ad22029f35180cd8d5eebc400d85ff9d6575eea
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-09-04 09:40:48 +0900

    feat: Change attach function parameter

--- Servo.py
+++ Servo.py
@@ -1,36 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
-
-class Servo():
-    def __init__(self):
+class Servo() :
+    def __init__(self) :
         self.GPIO = GPIO
         print('active init')
-
-    def attach(self, pin, hz):
+    
+    def attach(self, pin) :
         self.GPIO.setmode(self.GPIO.BCM)
         self.GPIO.setup(pin, self.GPIO.OUT)
-        self.pwm = GPIO.PWM(pin, hz)
+        self.pwm = GPIO.PWM(pin,50)
         print('active attach')
-
-    def start(self):
+        
+    def start(self) :
         self.pwm.start(0.52/20*100)
         print('active start')
-
-    def end(self):
+        
+    def end(self) :
         self.pwm.stop()
         self.GPIO.cleanup()
         print('active end')
-
-    def theta2duty(self, theta):
+    
+    def theta2duty(self,theta) :
         print('active theta2duty')
         return (0.52+0.0075*theta)*5
+        
 
-    def write(self, theta):
-        if theta > 270:
+    def write(self,theta) :
+        if theta>270 :
             raise ValueError('Theta must be less than 270')
-        if theta < 0:
+        if theta <0 :
             raise ValueError('Theta must be more than 0')
-
+        
         self.pwm.ChangeDutyCycle(self.theta2duty(theta))
         print('active write')
+
--- ds3235-profile.md
+++ ds3235-profile.md
@@ -1,3 +1,13 @@
+## ds3235-profile
+
 - cycle : 20ms
 - frequency : 40Hz
 - 0.5ms ~ 2.5ms
+
+## motor
+
+- motor 1 : GPIO14
+- motor 2 : GPIO15
+- motor 3 : GPIO18
+- motor 4 : GPIO23
+- motor 5 : GPIO24
