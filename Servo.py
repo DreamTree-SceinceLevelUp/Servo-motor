7c9fcb2af31c65574c1cf74de7a8aa69ce731af8
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-09-10 02:06:47 +0900

    fix: fix servo motor shaking problem using pigpio

--- Servo.py
+++ Servo.py
@@ -1,37 +1,26 @@
-import RPi.GPIO as GPIO
-import time
+import pigpio
+from time import sleep
 
 class Servo() :
     def __init__(self) :
-        self.GPIO = GPIO
-        print('active init')
-    
-    def attach(self, pin) :
-        self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setup(pin, self.GPIO.OUT)
-        self.pwm = GPIO.PWM(pin,50)
+        self.pi = pigpio.pi()
+        
+    def attach(self, pin,theta) :
+        self.pi.set_servo_pulsewidth(pin,0)
+        self.pi.set_servo_pulsewidth(pin,theta*7+560)
         print('active attach')
         
-    def start(self) :
-        self.pwm.start(0.52/20*100)
-        print('active start')
-        
-    def end(self) :
-        self.pwm.stop()
-        self.GPIO.cleanup()
-        print('active end')
-    
-    def theta2duty(self,theta) :
-        print('active theta2duty')
-        return (0.52+0.0075*theta)*5
-        
-
-    def write(self,theta) :
+    def write(self,pin,theta) :
         if theta>270 :
-            raise ValueError('Theta must be less than 270')
+            theta = 270
+            print('valueError: Theta must be less than 270')
         if theta <0 :
-            raise ValueError('Theta must be more than 0')
-        
-        self.pwm.ChangeDutyCycle(self.theta2duty(theta))
+            theta = 0
+            print('ValueError: Theta must be more than 0')
+        self.pi.set_servo_pulsewidth(pin,theta*7+560)
         print('active write')
-
+        
+        
+#servo = Servo()
+#servo.attach(14,0)
+#servo.write(14,180)
--- test2.py
+++ test2.py
@@ -1,8 +1,30 @@
 import Servo
 
-servo = Servo();
-servo.attach(18,50);
-servo.start()
+servo1 = Servo();
+servo2 = Servo();
+servo3 = Servo();
+servo4 = Servo();
+servo5 = Servo();
 
-servo.write(90)
-servo.end()
+servo1.attach(14);
+servo2.attach(15);
+servo3.attach(18);
+servo4.attach(23);
+servo5.attach(24);
+servo1.start()
+servo2.start()
+servo3.start()
+servo4.start()
+servo5.start()
+
+servo1.write(90)
+servo2.write(90)
+servo3.write(90)
+servo4.write(90)
+servo5.write(90)
+
+servo1.end()
+servo2.end()
+servo3.end()
+servo4.end()
+servo5.end()
